e6f9c0299cbf116038deb0573eee07f0175dce7e
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-02-02 20:08:35 -0800

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a single customers bank account
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Creates acc with specific owner and the balance of 0
+        /// </summary>
+        /// <param name="owner"></param>
+        public Account(string owner)
+        {
+            Owner = owner;
+        }
+
+        /// <summary>
+        /// The acc holder, full name
+        /// </summary>
+        public string Owner { get; set; }
+
+        /// <summary>
+        /// Amount of money in the account currently
+        /// </summary>
+        public double Balance { get; private set; }
+
+        /// <summary>
+        /// Gets the specified amount of money to the account
+        /// </summary>
+        /// <param name="amt"></param>
+        public void Deposit(double amt)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Amount of money took from the account
+        /// </summary>
+        /// <param name="amt"></param>
+        public void Withdraw(double amt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
